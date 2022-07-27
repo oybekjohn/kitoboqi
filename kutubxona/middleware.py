cd88304ad19d36af44abf68cbf5aa367520b642a
oybekjohn <oybeksjob@gmail.com>
2022-07-27 11:30:57 +0500

    add middleware as a test

---
+++ kutubxona/middleware.py
@@ -0,0 +1,10 @@
+class SimpleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        print(f'Ushbu |http://127.0.0.1:8000{request.path}| so\'rovidan OLDIN')
+        response = self.get_response(request)
+        print(f'Ushbu |http://127.0.0.1:8000{request.path}| so\'rovidan KEYIN')
+
+        return response
--- kutubxona/settings.py
+++ kutubxona/settings.py
@@ -43,6 +43,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'kutubxona.middleware.SimpleMiddleware',
 ]
 
 ROOT_URLCONF = 'kutubxona.urls'
