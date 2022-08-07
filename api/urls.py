c22a8169aa9cc80b41175de219d6159a54d7e89b
oybekjohn <oybeksjob@gmail.com>
2022-08-08 01:17:41 +0500

    API viewsets CRUD  example

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+# from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from api.views import BookReviewDetailAPIView, BookReviewAPIView
+# from api.views import BookReviewDetailAPIView, BookReviewAPIView
+from api.views import BookReviewsViewSet
 
 
 app_name = "api"
 
+router = DefaultRouter()
+router.register("reviews", BookReviewsViewSet, basename='nimadir') # routerni ro'yxatdan o'tkazish "reviews" bu url pathga
+
 urlpatterns = [
-    path("reviews/", BookReviewAPIView.as_view(), name="review-list"),
-    path("reviews/<int:id>/", BookReviewDetailAPIView.as_view(), name="review-detail"), # POST, GET, UPDATE, DELETE bor shu urlda
+    # path("reviews/", BookReviewAPIView.as_view(), name="review-list"),
+    # path("reviews/<int:id>/", BookReviewDetailAPIView.as_view(), name="review-detail"), # POST, GET, UPDATE, DELETE bor shu urlda
 ]
+urlpatterns +=router.urls
